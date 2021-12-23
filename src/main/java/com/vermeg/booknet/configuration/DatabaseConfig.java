2d01de86cba5cf89169cdb33efc8be814bb4cf07
Aymen Ben Abbes <aymen.benabbes@isamm.u-manouba.tn>
2021-12-23 21:50:50 +0100

    for deployement on heroku

---
+++ src/main/java/com/vermeg/booknet/configuration/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.vermeg.booknet.configuration;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	}
+}
